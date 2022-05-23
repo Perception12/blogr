42498f454fa25c745ec3cdd6271c0f3ceba8e9cb
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-05-23 23:33:34 +0100

    complete header

--- script.js
+++ script.js
@@ -3,24 +3,53 @@ var menuItem = document.getElementsByClassName('menu-item');
 for (let i = 0; i < menuItem.length; i++){
     menuItem[i].addEventListener('click', (event) => {
         let dropdown = event.target.querySelector('.dropdown');
-        toggleDropDown(dropdown);
+        let arrow = event.target.querySelector('.arrow');
+        toggleDropDown(dropdown, arrow);
+        toggleUnderline(event.target);
     })
 }
 
-var toggleDropDown = (element) => {
-    if (element.style.display=='block'){
-        element.style.display = 'none';
+var toggleDropDown = (dropdown, arrow) => {
+    toggleArrow(arrow);
+    if (dropdown.style.display=='block'){
+        dropdown.style.display = 'none';
     } else {
-        element.style.display = 'block';
-        turnOtherDropdown(element);
-    }
+        dropdown.style.display = 'block';
+        turnOtherDropdown(dropdown);
+    } 
 }
 
 var turnOtherDropdown = (element) => {
     for (let i = 0; i < menuItem.length; i++){
         dropdown = menuItem[i].querySelector('.dropdown');
+        downArrow = menuItem[i].querySelector('.arrow').children[0];
+        upArrow = menuItem[i].querySelector('.arrow').children[1];
         if (dropdown != element) {
             dropdown.style.display = 'none';
+            downArrow.style.display = 'inline-block';
+            upArrow.style.display = 'none';
+            menuItem[i].style.textDecoration = 'none';
         }
+    }
+}
+
+var toggleArrow = (element) => {
+    var down = element.children[0];
+    var up = element.children[1];
+
+    if (down.style.display != 'none'){
+        up.style.display = 'inline-block';
+        down.style.display = 'none';
+    } else {
+        up.style.display = 'none';
+        down.style.display = 'inline-block';
+    }
+}
+
+var toggleUnderline = (element) => {
+    if (element.style.textDecoration != 'underline'){
+        element.style.textDecoration = 'underline'
+    } else {
+        element.style.textDecoration = 'none';
     }
 }
